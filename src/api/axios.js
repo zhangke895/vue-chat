0af68a3df9e8c1d803b3fa3dcfaccdc908f1b9fe
zhangke895 <your_email@youremail.com>
2019-07-22 22:47:59 +0800

    'modify'

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,3 +2,12 @@ import axios from 'axios';
 const baseURL = '';
 const instance = axios.create();
 instance.defaults.timeout = 30000; // 所有接口30s超时
+// 请求统一处理
+instance.interceptors.request.use(async config => {
+    if (config.url && config.url.charAt(0) === '/') {
+        config.url = `${baseURL}${config.url}`;
+    }
+    return config;
+}, error => Promise.reject(error));
+
+export default instance;
